fdeae64a9ffeaf3b207dd4b2fbedf9e02b148906
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-05-22 18:30:24 +0200

    complete front page and product display

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,17 +31,17 @@ export default function Header() {
 
         {/* Right */}
         <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div className='link'>
+          <div className='amz-link'>
             <p>Hello Gwenaël Magnenat</p>
             <p className='font-extrabold md:text-sm'>Account & Lists</p>
           </div>
 
-          <div className='link'>
+          <div className='amz-link'>
             <p>Returns</p>
             <p className='font-extrabold md:text-sm'>& Orders</p>
           </div>
 
-          <div className='relative link flex items-center'>
+          <div className='relative amz-link flex items-center'>
             <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
               0
             </span>
@@ -55,19 +55,21 @@ export default function Header() {
 
       {/* Bottom nav */}
       <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
-        <p className='link flex items-center'>
+        <p className='amz-link flex items-center'>
           <MenuIcon className='h-6 mr-1' />
           All
         </p>
-        <p className='link'>Prime Video</p>
-        <p className='link'>Amazon Businesso</p>
-        <p className='link'>Today's Deals</p>
-        <p className='hidden link lg:inline-flex'>Electronics</p>
-        <p className='hidden link lg:inline-flex'>Food & Grocery</p>
-        <p className='hidden link lg:inline-flex'>Prime</p>
-        <p className='hidden link lg:inline-flex'>Buy Again</p>
-        <p className='hidden link lg:inline-flex'>Shopper Toolkit</p>
-        <p className='hidden link lg:inline-flex'>Health & Personal Care </p>
+        <p className='amz-link'>Prime Video</p>
+        <p className='amz-link'>Amazon Businesso</p>
+        <p className='amz-link'>Today's Deals</p>
+        <p className='hidden amz-link lg:inline-flex'>Electronics</p>
+        <p className='hidden amz-link lg:inline-flex'>Food & Grocery</p>
+        <p className='hidden amz-link lg:inline-flex'>Prime</p>
+        <p className='hidden amz-link lg:inline-flex'>Buy Again</p>
+        <p className='hidden amz-link lg:inline-flex'>Shopper Toolkit</p>
+        <p className='hidden amz-link lg:inline-flex'>
+          Health & Personal Care{' '}
+        </p>
       </div>
     </header>
   );
