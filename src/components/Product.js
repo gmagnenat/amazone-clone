69c4270d6f321472af5f72769347cdb12b5aef4d
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-05-22 09:51:59 +0200

    Add rating to product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,10 @@
+import { useState } from 'react';
 import Image from 'next/image';
+import { StarIcon } from '@heroicons/react/solid';
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
 export default function Product({
   id,
   title,
@@ -7,11 +13,28 @@ export default function Product({
   category,
   image,
 }) {
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+
+  const [hasPrime] = useState(Math.random() < 0.5);
   return (
     <div>
       <p>{category}</p>
 
       <Image src={image} height={200} width={200} objectFit='contain' />
+
+      <h4>{title}</h4>
+
+      <div className='flex'>
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className='h-5' />
+          ))}
+      </div>
+
+      <p>{description}</p>
     </div>
   );
 }
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,15 @@ export default function ProductFeed({ products }) {
   return (
     <div>
       {products.map(({ id, title, price, description, category, image }) => (
-        <Product key={id} id title price description category image />
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
       ))}
     </div>
   );
